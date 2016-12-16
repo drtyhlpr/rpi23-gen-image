a8dde7c8f0b412bfd158bf58988b25534793b929
Jean-Pierre Höhmann <jp@hhmn.de>
2016-12-16 02:12:06 +0100

    Fix AUTHORIZE_SSH_KEYS

--- bootstrap.d/60-authorize-keys.sh
+++ bootstrap.d/60-authorize-keys.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 . ./functions.sh
 
 #
@@ -8,9 +7,15 @@
 # build server, for the root account in the image, if AUTHORIZE_SSH_KEYS is set.
 #
 
-if [[ ${AUTHORIZE_SSH_KEYS} = true ]]
+if [ "${AUTHORIZE_SSH_KEYS}" = true ]
 then
     echo "Authorizing SSH keys..."
-    install -o root -g root -m 600 {~,${R}/root}/.ssh/authorized_keys
+    mkdir "${R}/root/.ssh/"
+    install \
+            -o root \
+            -g root \
+            -m 600 \
+            ~/.ssh/authorized_keys \
+            "${R}/root/.ssh/authorized_keys"
 fi
 
