605fd1fb030e91533eb4343796abffbf97c0bc4e
Jean-Pierre Höhmann <jp@hhmn.de>
2017-01-17 15:59:27 +0100

    Replace AUTHORIZE_SSH_KEYS with SSH_ROOT_KEYS

--- bootstrap.d/60-authorize-keys.sh
+++ bootstrap.d/60-authorize-keys.sh
@@ -1,13 +1,10 @@
 . ./functions.sh
 
 #
-# Authorize the same SSH users, that are authorized to use the build system.
-#
-# This script will authorize the SSH-keys authorized for the current user of the 
-# build server, for the root account in the image, if AUTHORIZE_SSH_KEYS is set.
+# Authorize SSH keys.
 #
 
-if [ "${AUTHORIZE_SSH_KEYS}" = true ]
+if [ -n "${SSH_ROOT_KEYS}" ]
 then
     echo "Authorizing SSH keys..."
     mkdir "${R}/root/.ssh/"
@@ -15,7 +12,7 @@ then
             -o root \
             -g root \
             -m 600 \
-            ~/.ssh/authorized_keys \
+            "${SSH_ROOT_KEYS}" \
             "${R}/root/.ssh/authorized_keys"
 fi
 
