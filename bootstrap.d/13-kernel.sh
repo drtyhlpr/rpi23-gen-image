e96075d9ada5e7c209ba96468bbbf12c820ea876
drtyhlpr <its_sead4@gmx.de>
2018-09-27 20:12:51 +0200

    fixed: No rule to make target 'firmware_install'
    sorry for the delay

--- bootstrap.d/13-kernel.sh
+++ bootstrap.d/13-kernel.sh
@@ -115,7 +115,9 @@ if [ "$BUILD_KERNEL" = true ] ; then
     make -C "${KERNEL_DIR}" ARCH="${KERNEL_ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" INSTALL_MOD_PATH=../../.. modules_install
 
     # Install kernel firmware
-    make -C "${KERNEL_DIR}" ARCH="${KERNEL_ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" INSTALL_FW_PATH=../../../lib firmware_install
+    if [ $(cat ./Makefile | grep "^firmware_install:") ] ; then
+      make -C "${KERNEL_DIR}" ARCH="${KERNEL_ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" INSTALL_FW_PATH=../../../lib firmware_install
+    fi
   fi
 
   # Install kernel headers
