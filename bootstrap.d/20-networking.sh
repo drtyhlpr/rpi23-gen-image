adcfe58416aa418091af991105ee503d17e3dd8e
Unknown <xyz258@abwesend.de>
2018-11-10 15:51:35 +0100

    FIX WIFI RPI 0W PART2

--- bootstrap.d/20-networking.sh
+++ bootstrap.d/20-networking.sh
@@ -127,7 +127,7 @@ if [ "$ENABLE_WIRELESS" = true ] ; then
   if [ "$RPI_MODEL" = 3P ] ; then
   chown root:root "${WLAN_FIRMWARE_DIR}/brcmfmac43455-sdio."*
   chmod 600 "${WLAN_FIRMWARE_DIR}/brcmfmac43455-sdio."*  
-  else
+  elif [ "$RPI_MODEL" = 3 ] || [ "$RPI_MODEL" = 0 ] ; then
   chown root:root "${WLAN_FIRMWARE_DIR}/brcmfmac43430-sdio."*
   chmod 600 "${WLAN_FIRMWARE_DIR}/brcmfmac43430-sdio."*
   fi
