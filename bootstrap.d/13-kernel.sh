71446b4553f8ca606b2cd8eb4124f56aee317c70
Unknown <xyz258@abwesend.de>
2018-12-07 05:53:52 +0100

    0

--- bootstrap.d/13-kernel.sh
+++ bootstrap.d/13-kernel.sh
@@ -106,14 +106,37 @@ if [ "$BUILD_KERNEL" = true ] ; then
         set_kernel_config CONFIG_Z3FOLD y
         set_kernel_config CONFIG_ZSMALLOC y
         set_kernel_config CONFIG_PGTABLE_MAPPING y
+		set_kernel_config CONFIG_LZO_COMPRESS y
 	  fi
 
       # enable basic KVM support; see https://www.raspberrypi.org/forums/viewtopic.php?f=63&t=210546&start=25#p1300453
 	  if [ "$KERNEL_VIRT" = true ] && { [ "$RPI_MODEL" = 2 ] || [ "$RPI_MODEL" = 3 ] || [ "$RPI_MODEL" = 3P ] ; } ; then
-        set_kernel_config CONFIG_VIRTUALIZATION y
+		set_kernel_config CONFIG_HAVE_KVM_IRQCHIP y
+        set_kernel_config CONFIG_HAVE_KVM_ARCH_TLB_FLUSH_ALL y
+        set_kernel_config CONFIG_HAVE_KVM_CPU_RELAX_INTERCEPT y
+        set_kernel_config CONFIG_HAVE_KVM_EVENTFD y
+        set_kernel_config CONFIG_HAVE_KVM_IRQFD y
+        set_kernel_config CONFIG_HAVE_KVM_IRQ_ROUTING y
+        set_kernel_config CONFIG_HAVE_KVM_MSI y
         set_kernel_config CONFIG_KVM y
-        set_kernel_config CONFIG_VHOST_NET m
+        set_kernel_config CONFIG_KVM_ARM_HOST y
+        set_kernel_config CONFIG_KVM_ARM_PMU y
+        set_kernel_config CONFIG_KVM_COMPAT y
+        set_kernel_config CONFIG_KVM_GENERIC_DIRTYLOG_READ_PROTECT y
+        set_kernel_config CONFIG_KVM_MMIO y
+        set_kernel_config CONFIG_KVM_VFIO y
+        set_kernel_config CONFIG_VHOST m
         set_kernel_config CONFIG_VHOST_CROSS_ENDIAN_LEGACY y
+        set_kernel_config CONFIG_VHOST_NET m
+        set_kernel_config CONFIG_VIRTUALIZATION y
+		
+		set_kernel_config CONFIG_MMU_NOTIFIER y
+		
+		# erratum
+		set_kernel_config ARM64_ERRATUM_834220 y
+		
+		# https://sourceforge.net/p/kvm/mailman/message/18440797/
+		set_kernel_config CONFIG_PREEMPT_NOTIFIERS y
 	  fi
 
       # enable apparmor,integrity audit,
