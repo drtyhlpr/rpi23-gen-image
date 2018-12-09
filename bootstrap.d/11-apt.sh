be99731b278c6a6944df32fd037bbbd068b74203
drtyhlpr <its_sead4@gmx.de>
2018-12-09 01:13:20 +0100

    quick fix: broken sources.list generation

--- bootstrap.d/11-apt.sh
+++ bootstrap.d/11-apt.sh
@@ -11,6 +11,13 @@ if [ -z "$APT_PROXY" ] ; then
   sed -i "s/\"\"/\"${APT_PROXY}\"/" "${ETC_DIR}/apt/apt.conf.d/10proxy"
 fi
 
+# Install APT sources.list
+install_readonly files/apt/sources.list "${ETC_DIR}/apt/sources.list"
+
+# Use specified APT server and release
+sed -i "s/\/ftp.debian.org\//\/${APT_SERVER}\//" "${ETC_DIR}/apt/sources.list"
+sed -i "s/ jessie/ ${RELEASE}/" "${ETC_DIR}/apt/sources.list"
+
 # Upgrade package index and update all installed packages and changed dependencies
 chroot_exec apt-get -qq -y update
 chroot_exec apt-get -qq -y -u dist-upgrade
