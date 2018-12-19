90a66357153e2fce407cfcac5960d6e5cc259c5e
Yannick Schinko <yannick@tedworld.de>
2018-12-19 12:27:51 +0100

    No need to check if the service exists
    If this snippet is included, the service exists

--- files/firstboot/23-restart-dphys-swapfile.sh
+++ files/firstboot/23-restart-dphys-swapfile.sh
@@ -1,7 +1,5 @@
 # Restart dphys-swapfile service if it exists
-if systemctl list-units | grep -q dphys-swapfile ; then
-  logger -t "rc.firstboot" "Restarting dphys-swapfile"
+logger -t "rc.firstboot" "Restarting dphys-swapfile"
 
-  systemctl enable dphys-swapfile
-  systemctl restart dphys-swapfile
-fi
+systemctl enable dphys-swapfile
+systemctl restart dphys-swapfile
