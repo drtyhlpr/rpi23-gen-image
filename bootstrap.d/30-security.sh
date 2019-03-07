6bee2fe4187fe925d257ac8b4d7b484120d1fc91
Jens <j.kapitza@schwarze-allianz.de>
2019-03-07 13:26:11 +0100

    change password setup
    mkpasswd dep. dropping

--- bootstrap.d/30-security.sh
+++ bootstrap.d/30-security.sh
@@ -5,20 +5,18 @@
 # Load utility functions
 . ./functions.sh
 
-# Generate crypt(3) password string
-ENCRYPTED_PASSWORD=$(mkpasswd -m sha-512 "${PASSWORD}")
-ENCRYPTED_USER_PASSWORD=$(mkpasswd -m sha-512 "${USER_PASSWORD}")
-
 # Setup default user
 if [ "$ENABLE_USER" = true ] ; then
   chroot_exec adduser --gecos "$USER_NAME" --add_extra_groups --disabled-password "$USER_NAME"
-  chroot_exec usermod -a -G sudo -p "${ENCRYPTED_USER_PASSWORD}" "$USER_NAME"
+  chroot_exec usermod -a -G sudo "$USER_NAME"
+  chroot_exec echo "'$USER_NAME:${USER_PASSWORD}' | chpasswd"
 fi
 
 # Setup root password or not
 if [ "$ENABLE_ROOT" = true ] ; then
-  chroot_exec usermod -p "${ENCRYPTED_PASSWORD}" root
+  chroot_exec echo "'root:${USER_PASSWORD}' | chpasswd"
 else
   # Set no root password to disable root login
-  chroot_exec usermod -p \'!\' root
+  chroot_exec usermod -L root
 fi
+
